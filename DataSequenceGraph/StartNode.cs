0db75df9fb3de92ad45ec2e30a8f789c00b6600e
ArtVandalay <ArtVandalay@example.com>
2011-01-23 16:37:42 -0500

    add start node

---
+++ DataSequenceGraph/StartNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataSequenceGraph
+{
+    public class StartNode : Node
+    {
+        public StartNode(int SequenceNumber) : base(SequenceNumber)
+        {
+        }
+    }
+}
